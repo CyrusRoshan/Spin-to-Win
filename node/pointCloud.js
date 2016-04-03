9b139ed23a2e660aa5d1afb3a43227be7f40bf7a
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-04-03 08:17:44 -0500

    Working point cloud coordinate generation

--- node/pointCloud.js
+++ node/pointCloud.js
@@ -1,3 +1,9 @@
+module.exports = {
+  pointCloud: pointCloud
+}
+
+const fs = require('fs');
+
 const VERTICAL_TIME = 55954; // ms
 const VERTICAL_DIST = 6.6 // cm
 const ROTATION_TIME = 4225.5 // ms
@@ -5,7 +11,9 @@ const DISTANCE_TO_CENTER = 4.2 //cm
 const TOTAL_ROTATIONS = VERTICAL_TIME / ROTATION_TIME; 
 
 function pointCloud(oneAxisDistanceTimeMap) { // [(time, distance_from_sensor), ...]
-  oneAxisDistanceTimeMap.map((time, distanceFromSensor) => {
+  return oneAxisDistanceTimeMap.map(val => {
+    const time = val.time;
+    const distanceFromSensor = val.distance;
     const distanceFromCenter = DISTANCE_TO_CENTER - distanceFromSensor;
     const cartesianTime = time % ROTATION_TIME;
     const cartesianCoor = get2DCoor(distanceFromCenter, cartesianTime, ROTATION_TIME);
@@ -23,3 +31,22 @@ function get2DCoor(distanceFromCenter, time, rotTime) {
   const py = distanceFromCenter * Math.sin(angle);
   return [px, py];
 }
+
+function outputFormat(data) {
+  var init = data.reduce((prev, curr) => {
+    return prev.concat(
+      `{${curr[0]},${curr[1]},${curr[2]}},`
+    )
+  }, '{')
+
+  var bracketFinal = init.split('')
+  bracketFinal[init.length - 1] = '}';
+  bracketFinal = bracketFinal.join('')
+  return bracketFinal;
+}
+
+var data = fs.readFileSync('../test.dat');
+data = JSON.parse(data);
+var dataMap = pointCloud(data);
+dataMap = outputFormat(dataMap);
+fs.writeFile('../final.txt', dataMap)
